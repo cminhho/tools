338a6d8724ea3b82bd6960120984c8cfab55b9d6
cminhho <hmchung92@gmail.com>
2015-03-05 10:03:53 +0700

    chore(app): update app

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log('This would be the main JS file.');
+ var iframe = document.getElementById('iframe');
+      var angularapp = angular.module('myApp', ['ngSanitize']);
+      angularapp.controller('FieldCtrl', function ($scope, $sce) {
+        var iframeclass = '';
+        $scope.loadTemplate = function() {
+          if ($scope.template.length > 0) {
+            // add iframe classs
+            iframeclass = $scope.template.split('.')[0];
+            iframe.classList.add(iframeclass);
+            $scope.activeTemplate = $sce.trustAsResourceUrl($scope.template);
+          } else {
+            iframe.classList.remove(iframeclass);
+          };
+        };
+      });
+      // custom directive
+      angularapp.directive('myChange', function() {
+        return function(scope, element) {
+          element.bind('input', function() {
+            // the iframe function
+            iframe.contentWindow.update({
+              name: element[0].name,
+              value: element[0].value
+            });
+          });
+        };
+      });
